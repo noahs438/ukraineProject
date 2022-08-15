687b5a09d5c81c56e691063e11e5f84e4c3d8e5d
noahserhan <21nas6@queensu,ca>
2022-08-14 21:36:23 -0400

    Making posts editable

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -34,9 +34,9 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;
     const post = req.body;
 
-    if(!mongoose.Types.objectId.isValid(_id)) return res.status(404).send('No post with that ID')
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that ID')
 
-    const updatedPost = await PostMessage.fingByIdAndUpdate(_id, post, { new: true })
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
 
     res.json(updatedPost);
 }
