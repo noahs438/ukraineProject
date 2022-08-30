4e936869df479bdad00d13dab9a03d103f6d6756
noahserhan <21nas6@queensu,ca>
2022-08-30 14:00:34 -0400

    Working on Google OAuth

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import {Container, Grid, Grow} from "@material-ui/core";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import useStyles from "../../styles";
+import {useDispatch} from "react-redux";
+import {getPosts} from "../../actions/posts";
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();  // Hook
+
+    useEffect(() => {
+        dispatch(getPosts());  // As soon as the action gets dispatched, we go to the posts reducer to handle the logic
+    }, [currentId, dispatch]);
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid className={classes.mainContainer} container justifyContent={"space-between"} alignItems={"stretch"} spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    );
+}
+
+
+export default Home;
